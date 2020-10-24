f5481c49ea5ea98c687e3d405eb01083ecf100a6
user <user@debian>
2020-10-24 10:38:57 +0200

    Major update mobile

--- frontend/src/Generics.js
+++
@@ -1,9 +0,0 @@
-import jwt from  'jsonwebtoken'
-class Generics{
-    static isAuthenticated() {
-        const payload = jwt.decode(localStorage.getItem('refresh_token'))
-        const now = Math.round(Date.now() / 1000)
-        return payload && now < payload.exp
-    }
-}
-export default Generics

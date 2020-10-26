d86440d448d66abcdbec5315b560eb4e91e67228
user <user@debian>
2020-10-26 12:07:12 +0100

    Tests

--- api/tests.py
+++ api/tests.py
@@ -1,2 +1,14 @@
 
-        
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+from .models import *
+
+class Test(APITestCase):
+    def notLogged(self):
+        self.client = APIClient()
+        url = 'http://localhost:8000/api/products/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
